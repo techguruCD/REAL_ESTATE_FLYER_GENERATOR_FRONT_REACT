0d7b9ee4576d9443beae2175e35d49081652baaa
techguruCD <colinkong0406@yahoo.com>
2023-11-29 04:18:10 -0300

    initial_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { RadioGroup } from '@headlessui/react'
+// import logo from './logo.svg';
 import './App.css';
+
+const plans = [
+  { name: 'Startup', priceMonthly: 29, priceYearly: 290, limit: 'Up to 5 active job postings' },
+  { name: 'Business', priceMonthly: 99, priceYearly: 990, limit: 'Up to 25 active job postings' },
+  { name: 'Enterprise', priceMonthly: 249, priceYearly: 2490, limit: 'Unlimited active job postings' },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='min-h-screen min-w-screen bg-[#111022]'>
+      <div className='px-10 py-2 w-full flex justify-between'>
+        <div>FLYER</div>
+        <div>Hello Sunny Vo!</div>
+      </div>
+      <div className='flex flex-col w-full items-center'>
+        <div className='max-w-xl  text-center'>
+          <h1 className='text-white text-4xl'>
+            Real Estate Flyer Made in Seconds
+          </h1>
+          <h2 className='text-gray-400 text-sm mt-3'>
+            For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.
+          </h2>
+        </div>
+
+        <div className='max-w-xl mt-8 w-full'>
+          <div className='w-full flex'>
+            <div className='grow text-sm font-semibold text-center text-white rounded-md bg-gradient-to-r from-[#586FFB] to-[#8D79FB] px-3.5 py-2.5 hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'>
+              Listed
+            </div>
+            <div className='grow text-sm font-semibold text-center text-white rounded-md bg-gradient-to-r from-[#586FFB] to-[#8D79FB] px-3.5 py-2.5 hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'>
+              Not Listed
+            </div>
+          </div>
+          <div className='max-w-xl mt-8 w-full p-6'>
+            <div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(169.40% 89.55% at 94.76% 6.29%, rgba(0, 0, 0, 0.40) 0%, rgba(255, 255, 255, 0.00) 100%)',
+      }
+    },
+  },
+  plugins: [],
+}
+
